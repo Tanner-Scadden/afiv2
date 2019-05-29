4f629e8bbc309f621d4f7e5f9dc7d6f2faa7b463
Tanner Scadden <tanner@scaddenfamily.com>
2019-05-29 16:48:19 -0600

    finished v2 of homepage

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import './Header.scss';
+import Logo from '../../Images/AFI-Logo.png';
+
+const Header = (props) => {
+
+  const [display, toggleDisplay] = useState(false);
+  const [menu, toggleMenu] = useState(false);
+
+  useEffect(() => {
+    return () => {
+      return display
+    };
+  }, [display])
+
+
+  function displayMenu() {
+
+  }
+
+  return (
+    <header>
+
+      <nav className="container">
+
+        <div className="left">
+          <img src={Logo} alt="logo" />
+          <h1>AFI PAINT & SUPPLY</h1>
+        </div>
+
+        <div className="right">
+          {display
+            ? <p onClick={() => { toggleDisplay(false); toggleMenu(false) }} className="X mobile">&#10005;</p>
+            : <p onClick={() => { toggleDisplay(true); toggleMenu(true) }} className="hamburger mobile">&#9776;</p>
+          }
+          <div className="nav-menu desktop">
+            <Link to="/" className="item"><h3>Home</h3></Link>
+            <Link to="/biography" className="item"><h3>Bio</h3></Link>
+          </div>
+        </div>
+        {menu
+          ? <ul className="menu mobile"><Link to="/" className="item"><li>Home</li></Link><Link to="/biography" className="item"><li>Bio</li></Link></ul>
+          : null
+        }
+
+      </nav>
+
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/Pages/Home.js
@@ -0,0 +1,135 @@
+import React from 'react';
+
+
+// Components
+import Header from '../Components/Header/Header';
+
+// Styling
+import './Home.scss';
+
+// Material UI
+import { TextField } from '@material-ui/core';
+
+// IMAGES //
+
+// BRAND IMAGES //
+import NorthStar from '../Images/brands/NorthStar.png';
+import GenTec from '../Images/brands/GenTec.png';
+import LixX from '../Images/brands/LixX.png';
+import GenRock from '../Images/brands/GenRock.png';
+import Gen20 from '../Images/brands/Gen20.png';
+import SunMight from '../Images/brands/SunMight.jpg';
+import PlioGrip from '../Images/brands/PlioGrip.jpg';
+import Anest from '../Images/brands/Anest.jpg';
+import USC from '../Images/brands/USC.jpg';
+import AutomotiveArt from '../Images/brands/Automotive-Art.png';
+import AFI from '../Images/brands/AFI.png';
+// // 
+
+// DIVIDER IMAGES //
+import car1 from '../Images/oldcar1.jpg';
+import car2 from '../Images/oldcar2.jpg';
+import car3 from '../Images/oldcar3.jpg';
+
+
+const Home = (props) => {
+  return (
+    <div>
+      <Header />
+      <div className="home">
+
+        <div className="top">
+          <div className="banner">
+            <span className="title">WELCOME TO AFI</span>
+            <div className="info">
+              <span><i class="fas fa-phone"></i>(801) 990-7360</span>
+              <span><i class="far fa-envelope"></i>info@afipaintsupply.com</span>
+            </div>
+            <div class="arrow bounce">
+              <h4>See Our Products</h4>
+              <a class="fa fa-arrow-down fa-2x" href="#"></a>
+            </div>
+          </div>
+        </div>
+
+        <div className="products">
+          <h3 className="section-title">Product Lines</h3>
+          <ul>
+            <li><img src={NorthStar} alt="brand" /></li>
+            <li><img src={GenTec} alt="brand" /></li>
+            <li><img src={LixX} alt="brand" /></li>
+            <li><img src={GenRock} alt="brand" /></li>
+            <li><img src={Gen20} alt="brand" /></li>
+            <li><img src={SunMight} alt="brand" /></li>
+            <li><img src={PlioGrip} alt="brand" /></li>
+            <li><img src={Anest} alt="brand" /></li>
+            <li><img src={USC} alt="brand" /></li>
+            <li><img src={AutomotiveArt} alt="brand" /></li>
+            <li><img src={AFI} alt="brand" /></li>
+          </ul>
+        </div>
+
+        <div className="divider">
+          <img src={car1} alt="car" />
+          <img src={car2} alt="car" />
+          <img src={car3} alt="car" />
+        </div>
+
+        <div className="contact-container">
+          <div className="contact">
+            <h3 className="section-title">Contact Us</h3>
+            <div className="form-container">
+              <form method="post" name="contact" action="/success" data-netlify="true" netlify-honeypot="bot-field">
+                <input
+                  type="hidden"
+                  name="form-name"
+                  value="contact"
+                />
+                <div className="field half first">
+                  <TextField
+                    required
+                    id="outlined-required"
+                    label="Email"
+                    margin="normal"
+                    variant="outlined"
+                    fullWidth={true}
+                  />
+                </div>
+                <div className="field half">
+                  <TextField
+                    required
+                    id="outlined-required"
+                    label="Name"
+                    margin="normal"
+                    variant="outlined"
+                    fullWidth={true}
+                  />
+                </div>
+                <div className="field">
+                  <TextField
+                    required
+                    id="outlined-required"
+                    label="Message"
+                    margin="normal"
+                    variant="outlined"
+                    multiline={true}
+                    rows={3}
+                    rowsMax={5}
+                    fullWidth={true}
+                  />
+                </div>
+                <ul className="actions">
+                  <li><button type="reset" value="Reset" className="reset">Reset</button></li>
+                  <li><button type="submit" value="Send Message" className="submit">Send Message</button></li>
+                </ul>
+              </form>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home;
